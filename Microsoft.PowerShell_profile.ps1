b4189f9661f859f22f6fa904c5e5bbe63582f718
Jay Bazuzi <jbazuzi@tableau.com>
2017-09-17 10:56:36 -0700

    profile

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,80 @@
+function GetNextHistoryItemId
+{
+    if (Get-History) {
+        return (Get-History | select -Last 1).id + 1
+    } else {
+        return 1;
+    }
+}
+
+function GetLastHistoryItemDuration
+{
+    $lastHistoryItem = Get-History | select -Last 1
+    if ($lastHistoryItem) {
+        return ($lastHistoryItem.EndExecutionTime - $lastHistoryItem.StartExecutionTime)
+    } else {
+        return $null;
+    }
+}
+
+function prompt
+{
+    # if the last command didn't give us a newline, take one.
+    if ($host.ui.RawUI.CursorPosition.X) { Write-Host }
+
+    # If the last command took a long time, we want to report the delay
+    $lastHistoryItemDuration = GetLastHistoryItemDuration
+    if($lastHistoryItemDuration -and $lastHistoryItemDuration.TotalSeconds -gt 1) {
+        $delayMessage = " {0:00}:{1:00}:{2:00}" -f ($lastHistoryItemDuration.Hours, $lastHistoryItemDuration.Minutes, $lastHistoryItemDuration.Seconds)
+    }
+
+    # make a red line between commands, with the delay at the end.
+    $width = ($Host.UI.RawUI.WindowSize.Width - 2 - $(Get-Location).ToString().Length) - $delayMessage.Length
+    Write-Host -NoNewline -ForegroundColor Red $(Get-Location)
+    Write-Host -NoNewline " "
+    Write-Host -NoNewline -ForegroundColor Red (New-Object System.String @('-',$width))
+    Write-Host -NoNewline -ForegroundColor Yellow $delayMessage
+    Write-Host ""
+
+    $global:LASTEXITCODE = $realLASTEXITCODE
+    "#$(GetNextHistoryItemId) PS> "
+}
+
+function .. { cd .. }
+
+$env:path += ';' + (split-path $profile)
+
+### tableau stuff
+
+sal tube                tableau-tools/pipeline/tube.py
+sal tcproof             tableau-tools/pipeline/tcproof.py
+sal gsub                tableau-tools/pipeline/gsub.py
+sal sourceSync          tableau-tools/SourceAnalyzers/sourceSync.py
+sal format_opened       tableau-1.3/tools/clang_format_opened.py 
+sal tableau             tableau-1.3/build/Release-x64/tableau.exe
+
+function Exec-Block([string]$cmd) {
+    Write-Host -ForegroundColor Yellow "$cmd"
+    & ([scriptblock]::Create($cmd))
+
+    # Need to check both of these cases for errors as they represent different items
+    # - $?: did the powershell script block throw an error
+    # - $lastexitcode: did a windows command executed by the script block end in error
+    if ((-not $?) -or ($lastexitcode -ne 0)) {
+        throw "Command failed to execute: $cmd"
+    } 
+}
+
+function ttt($modules, $test, $config='Release')  {
+    $modules | foreach {
+        Exec-Block "tube --quiet --target test_$_"
+    }
+
+    if (!$?) {
+        exit
+    }
+
+    $modules | foreach {
+        Exec-Block ([scriptblock]::Create(" & tableau-1.3\build\$config-x64\test_$_.exe $test" ))
+    }
+}
