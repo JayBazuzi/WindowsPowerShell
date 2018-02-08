8b61099294462e69b073c9447d2f9c3673e1911a
Jay Bazuzi <jbazuzi@tableau.com>
2018-02-08 09:56:00 -0800

    add killtableau

---
+++ killtableau.cmd
@@ -0,0 +1,27 @@
+@echo off
+for %%i in (
+    tableau.exe
+    java.exe
+    javaw.exe
+    jruby.exe
+    tabspawnde.exe
+    tabspawn.exe
+    unittest.exe
+    tdeserver64.exe
+    tabsvc.exe
+    httpd.exe
+    backgrounder.exe
+    tabrepo.exe
+    postgres.exe
+    wgserver.exe
+    vizqlserver.exe
+    tabsystray.exe
+    dataserver.exe
+    tabsvcmonitor.exe
+    lmgrd.exe
+    tabsrvlic.exe
+    ruby.exe
+    tabadmin.exe
+    hostedproxy.exe
+    remoteagentserver.exe    
+) do taskkill /f /im %%i
